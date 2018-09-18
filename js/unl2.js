c6a888a9842949c04be145119d888ef9e8f9bc57
Robert Lafferty <rclafferty@ksu.edu>
2018-09-18 08:29:03 -0500

    Even More Content
    Added some duties, description text, and more pictures

--- js/unl2.js
+++ js/unl2.js
@@ -50,7 +50,7 @@ programming_exe_links = [
     "#"
 ];
 programming_text = [ 
-    "CryoCrypt was my first significant step into game development. I worked alongside 3 other team members at the 2015 Kansas State University Game Jam to create this game in a span of 48 hours. As a team, we started with brainstorming storyboard ideas, and then quickly moved into programming, art, and so forth. 2 students used Adobe Photoshop to hand-craft the art for characters and scenery. Another team member and myself both focused on using Unity C# scripts in order to program the puzzles, algorithms, movement, timers, and more. Myself and 1 of the artists also recorded voices for the characters using Audacity. Unfortunately, the voice-overs are not implemented in this build.",
+    "CryoCrypt was my first significant step into game development. I worked alongside three other team members at the 2015 Kansas State University Game Jam to create this game in a span of 48 hours. As a team, we started with brainstorming storyboard ideas, and then quickly moved into programming, art, and so forth. Two students used Adobe Photoshop to hand-craft the art for characters and scenery. Another team member and myself both focused on using Unity C# scripts in order to program the puzzles, algorithms, movement, timers, and more. Myself and one of the artists also recorded voices for the characters using Audacity. Unfortunately, the voice-overs are not implemented in this build.",
     "Sand was the product of my most recent Game Jam, which was Texas A&M University's Chillennium 2016. Teams from different parts of the country gathered to compete in this Game Jam and as a result, created some awesome games. My team consisted of 1 artist and 3 programmers. All aspects of this game, excluding audio, were made by hand. I found and implemented open source audio files for the background music as well as sound effects. 1 student used Adobe Photoshop to create the art. Myself and 3 others used Unity C# scripts to program character movement, enemy activity, interactivity, controls, and more. Everyone collaborated closely to develop gameplay, theme, and other overall aspects of the game.",
     "Securing Cyberville was created as a senior project at Kansas State University. I worked alongside 1 other team member throughout the semester to create this game. As a team, we started with researching how people learn about cyber security threats, and used that to brainstorm gameplay ideas, map the world, and so forth. The game was created using the Unity game engine, Creative Commons models, and Creative Commons textures.",
     "[Text about YGGDRASIL here]",
@@ -61,9 +61,15 @@ programming_text = [
     "[Text about Game Dev Boilerplate Code here]"
 ];
 programming_duties = [
-    "",
-    "",
-    ""
+    "Creating scripts to control user events, environments (such as timing out), storyboarding, recording voice-over audio, assisting in level design",
+    "Finding open-licensed music, player controls, multiplayer controls, level design",
+    "Level design, vehicle mechanics, dynamically assigning UI choices, UI design, assist with storyboarding and gameplay design",
+    "#",
+    "#",
+    "#",
+    "#",
+    "#",
+    "#"
 ];
 
 websitesIndex = 0;
@@ -94,12 +100,14 @@ websites_imgs = [
 websites_text = [ 
     "This History of Overland Park website was created as an assignment for my CWEB 105 course at Johnson County Community College. This course used Dreamweaver CS6 to teach students the basics of HTML5 and CSS6. The class assignments focused on molding the style and formatting of websites, specifcally in in the manner you see in this website. The instructor provided us as students with pre-constructed text files containing information about John Overland Park, Betsy Lenexa, inventions of the late 1800s, and more that we used in order to create the web pages. This class focused a lot on HTML as relates to using Dreamweaver and not the HTML itself, but rightfully so. I quickly realized that Dreamweaver is a great tool for aiding in web development and I enjoyed using it.",
     "This website about the My Lai Massacre was created as a final project for my CWEB 105 class at Johnson County Community College. In that class, I was tasked with researching a topic of my own interest and creating a full website using that research. I chose to research the My Lai (pronounced \"mee lahy\") Massacre during the Vietnam War. I am a bit of a \"history buff\" and had heard about this event shortly before the assignment. My research included information about key individuals, groups involved, events of the massacre, and the reprecussions that followed the event. I chose a black and white color scheme to reflect the ominous tone of the event.",
-    "[Text about Securing Cyberville Web Application Website here]",
+    "The web application for Securing Cyberville was developed as part of a research project for an independent study. I took my senior project and logged certain events to a database, including click events, scene changes, picking up items, etc. I then was tasked with making a web application to read from the database and display these events in a searchable and sortable fashion. It was my first time ever creating a web application like this, with the added difficulty of doing so from a Unity game. The task had a deadline of two weeks, but I completed this in one week.",
     "[Text about Paper Summary Website here]",
     "[Text about Responsive CSS Grid Website here]",
     "[Text about Personal Website here]"
 ];
 websites_duties = [
+    "",
+    "",
     "",
     "",
     "",
@@ -142,7 +150,7 @@ function setProgrammingDetails(index)
     document.getElementById("programmingImage").src = programming_imgs[index];
     document.getElementById("programmingImage").alt = "Image for " + programming_titles[index];
     document.getElementById("programmingText").textContent = programming_text[index];
-    document.getElementById("programmingDuties").textContent = 'My contributions included: ' + programming_duties[index];
+    document.getElementById("programmingDuties").textContent = programming_duties[index];
 }
 
 function showOnGitHub()
@@ -196,7 +204,7 @@ function setWebsitesDetails(index)
     document.getElementById("websitesImage").src = websites_imgs[index];
     document.getElementById("websitesImage").alt = "Image for " + websites_titles[index];
     document.getElementById("websitesText").textContent = websites_text[index];
-    document.getElementById("websitesDuties").textContent = 'My contributions included: ' + websites_duties[index];
+    document.getElementById("websitesDuties").textContent = websites_duties[index];
 
     if (websiteTitle == "My Personal Website")
     {
