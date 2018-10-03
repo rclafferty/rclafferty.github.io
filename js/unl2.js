da4297cf15bfa7e0ed2653a131728d4e66ac5d29
Robert Lafferty <rclafferty@k-state.edu>
2018-10-03 02:27:51 -0500

    (Almost) Finalized Home, Projects, and About
    Added lots of Home and About content and tweaked to fit new style
    
    Almost finished with overall design, but need to make sure the style is consistent on different resolutions.
    
    Almost done with projects content -- this is part of the consistency issues.

--- js/unl2.js
+++ js/unl2.js
@@ -25,7 +25,8 @@ const programs = [
         "#",
         "images/programming/cryocrypt.png",
         "http://people.cs.ksu.edu/~rclafferty/project_downloads/programming/games/CryoCrypt.zip",
-        "CryoCrypt was my first significant step into game development. I worked alongside three other team members at the 2015 Kansas State University Game Jam to create this game in a span of 48 hours. As a team, we started with brainstorming storyboard ideas, and then quickly moved into programming, art, and so forth. Two students used Adobe Photoshop to hand-craft the art for characters and scenery. Another team member and myself both focused on using Unity C# scripts in order to program the puzzles, algorithms, movement, timers, and more. Myself and one of the artists also recorded voices for the characters using Audacity. Unfortunately, the voice-overs are not implemented in this build.",
+        // "CryoCrypt was my first significant step into game development. I worked alongside three other team members at the 2015 Kansas State University Game Jam to create this game in a span of 48 hours. As a team, we started with brainstorming storyboard ideas, and then quickly moved into programming, art, and so forth. Two students used Adobe Photoshop to hand-craft the art for characters and scenery. Another team member and myself both focused on using Unity C# scripts in order to program the puzzles, algorithms, movement, timers, and more. Myself and one of the artists also recorded voices for the characters using Audacity. Unfortunately, the voice-overs are not implemented in this build.",
+        "CryoCrypt was developed by myself and three others at the 2015 Kansas State University Game Jam, created and prepared for judging in under 48 hours based on a given theme: \"Securing the Future.\" Two teammates used Adobe Photoshop to hand-craft the art for characters and scenery while myself and one other teammate focused on using Unity C# scripts to program the puzzles, algorithms, movement, timers, and more.",
         "Creating scripts to control user events, environment events (such as timing out), storyboarding, recording voice-over audio, assisting in level design"
     ],
 
@@ -35,7 +36,8 @@ const programs = [
         "#",
         "images/programming/sand.png",
         "#",
-        "Sand was the product of my most recent Game Jam, which was Texas A&M University's Chillennium 2016. Teams from different parts of the country gathered to compete in this Game Jam and as a result, created some awesome games. My team consisted of 1 artist and 3 programmers. All aspects of this game, excluding audio, were made by hand. I found and implemented open source audio files for the background music as well as sound effects. 1 student used Adobe Photoshop to create the art. Myself and 3 others used Unity C# scripts to program character movement, enemy activity, interactivity, controls, and more. Everyone collaborated closely to develop gameplay, theme, and other overall aspects of the game.",
+        // "Sand was the product of my most recent Game Jam, which was Texas A&M University's Chillennium 2016. Teams from different parts of the country gathered to compete in this Game Jam and as a result, created some awesome games. My team consisted of 1 artist and 3 programmers. All aspects of this game, excluding audio, were made by hand. I found and implemented open source audio files for the background music as well as sound effects. 1 student used Adobe Photoshop to create the art. Myself and 3 others used Unity C# scripts to program character movement, enemy activity, interactivity, controls, and more. Everyone collaborated closely to develop gameplay, theme, and other overall aspects of the game.",
+        "Sand was developed by myself and three others at the 2016 Texas A&M Chillennium Game Jam, created and prepared for judging in under 48 hours. All aspects of this game except the music were made by hand. I found and implemented open source audio files for background music and sound effects. One teammate used Adobe Photoshop to create the art while myself and three others used Unity C# scripts to program character movement, enemy activity, interactivity, controls, and more.",
         "Finding open-licensed music, player controls, multiplayer controls, level design"
     ],
 
@@ -131,7 +133,7 @@ const websites = [
         "History of Overland Park",
         "http://people.cs.ksu.edu/~rclafferty/prev_websites/overlandpark/",
         "images/websites/overland_park.png",
-        "This History of Overland Park website was created as an assignment for my CWEB 105 course at Johnson County Community College. This course used Dreamweaver CS6 to teach students the basics of HTML5 and CSS6. The class assignments focused on molding the style and formatting of websites, specifcally in in the manner you see in this website. The instructor provided us as students with pre-constructed text files containing information about John Overland Park, Betsy Lenexa, inventions of the late 1800s, and more that we used in order to create the web pages. This class focused a lot on HTML as relates to using Dreamweaver and not the HTML itself, but rightfully so. I quickly realized that Dreamweaver is a great tool for aiding in web development and I enjoyed using it.",
+        "This History of Overland Park website was created as an assignment for my CWEB 105 course at Johnson County Community College. This course used Dreamweaver CS6 to teach students the basics of HTML5 and CSS6. The class assignments focused on molding the style and formatting of websites, specifcally in in the manner you see in this website. The instructor provided us as students with pre-constructed text files that we used in order to create the web pages. This class focused a lot on HTML as relates to using Dreamweaver.",
         "Layout design, implement HTML/CSS design specifications using given content"
     ],
 
@@ -149,7 +151,7 @@ const websites = [
         "Securing Cyberville Web Application",
         "http://people.cs.ksu.edu/~rclafferty/securing_cyberville/",
         "images/websites/securing_cyberville.jpg",
-        "The web application for Securing Cyberville was developed as part of a research project for an independent study. I took my senior project and logged certain events to a database, including click events, scene changes, picking up items, etc. I then was tasked with making a web application to read from the database and display these events in a searchable and sortable fashion. It was my first time ever creating a web application like this, with the added difficulty of doing so from a Unity game. The task had a deadline of two weeks, but I completed this in one week.",
+        "The web application for Securing Cyberville was developed as part of a research project for an independent study. I took my senior project and logged certain events to a database, including click events, scene changes, picking up items, etc. I then was tasked with making a web application to read from the database and display these events in a searchable and sortable fashion. It was my first time ever creating a web application like this, with the added difficulty of doing so from a Unity game.",
         "Utilizing a pre-existing project to fulfill a research need, database development, PHP script development (to interact between database and web app), HTML/CSS development, searching and sorting functionality"
     ],
 
@@ -158,7 +160,7 @@ const websites = [
         "Paper Summary Website",
         "http://people.cs.ksu.edu/~rclafferty/weeklyLogs/",
         "images/websites/weekly_logs.png",
-        "[Text about Paper Summary Website here]",
+        "The Paper Summary Website was developed as a side project to continue exploring PHP web application development. This website pulls information form a database and displays it to the page. The papers that are being summarized are as part of a independent study I took in Fall 2017, with the feedback being from the instructor.",
         "PHP development, HTML/CSS design, database design, content development"
     ],
 
@@ -167,7 +169,7 @@ const websites = [
         "Responsive CSS Grid Website",
         "https://github.com/rclafferty/Responsive-CSS-Grid-Website",
         "images/websites/responsive_css_website.png",
-        "[Text about Responsive CSS Grid Website here]",
+        "The CSS Grid website was developed as part of a tutorial on Udemy that I was taking to learn JavaScript. It did not use any particular framework, but instead gave a broad intro to JavaScript, HTML, and CSS Grid development.",
         "Layout design, implement HTML/CSS design specifications using given content, JavaScript development, UI implementation"
     ],
 
@@ -176,7 +178,7 @@ const websites = [
         "My Personal Website",
         "https://github.com/rclafferty/rclafferty.github.io",
         "images/websites/personal_website.png",
-        "[Text about Personal Website here]",
+        "My personal website is one that is both a project and a portfolio. I've hand-written all of the HTML5, CSS3, JS, and PHP code used to construct the designs over the years with very few exceptions. I also use it to showcase my projects, both in programming and web development.",
         "HTML/CSS design, JavaScript/PHP development, content development"
     ]
 ];
@@ -211,7 +213,7 @@ function programmingPrevious()
 
 function setProgrammingDetails(program)
 {
-    document.getElementById("programmingProjectTitle").textContent = program[PROGRAMMING_TITLE_INDEX];
+    document.getElementById("programmingProjectTitle").textContent = program[PROGRAMMING_TITLE_INDEX] + " - " + (programmingIndex + 1) + "/" + NUMBER_OF_PROGRAMS;
     document.getElementById("programmingImage").src = program[PROGRAMMING_IMAGES_INDEX];
 
     if (program[PROGRAMMING_IMAGES_INDEX] == PHOTO_NOT_AVAILABLE)
@@ -286,7 +288,7 @@ function setWebsitesDetails(website)
         document.getElementById("websiteLinkButton").textContent = "View Website";
     }
 
-    document.getElementById("websitesProjectTitle").textContent = websiteTitle;
+    document.getElementById("websitesProjectTitle").textContent = websiteTitle + " - " + (websitesIndex + 1) + "/" + NUMBER_OF_WEBSITES;
     document.getElementById("websitesImage").src = website[WEBSITES_IMAGES_INDEX];
     document.getElementById("websitesImage").alt = "Image for " + websiteTitle;
     document.getElementById("websitesText").textContent = website[WEBSITES_TEXT_INDEX];
